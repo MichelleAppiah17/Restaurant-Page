8912e3e51e0a0b90abfcd10c58175ffef9d842e3
Michelle Appiah <michappiah67@gmail.com>
2023-08-30 09:36:19 +0000

    images

--- dist/food.js
+++ dist/food.js
@@ -1,5 +1,12 @@
-const menuLink = document.getElementsByClassName('menu');
 
-menuLink.addEventListener('click', functiom menuInfo(){
-    
-})
+const menuLinks = document.querySelectorAll('.menu');
+
+menuLinks.forEach(function(menuLink) {
+  menuLink.addEventListener('click', function menuImages() {
+    const fufuImage = document.createElement('img');
+    fufuImage.src = "./static/fufuuu.jpeg"
+
+    const menuLinkContainer = document.querySelector('.menu-container');
+    menuLinkContainer.appendChild(fufuImage);
+  });
+});
