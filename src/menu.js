7547db9ca83936c924eb0e939bfc630dd7d422fe
Michelle Appiah <michappiah67@gmail.com>
2023-08-31 00:31:47 +0000

    menu tab

--- src/menu.js
+++ src/menu.js
@@ -2,160 +2,103 @@
 function menuPage(){
    // menuButton.addEventListener('click', function() {
         const menuContainer = document.createElement('div');
-        menuContainer.style.backgroundImage = 'url("./static/images/cake.jpeg")';
-        menuContainer.style.backgroundSize = '100%'; 
-        menuContainer.style.backgroundPosition = 'center center'
-        menuContainer.style.backgroundRepeat = 'no-repeat';
-       // tabsDiv.appendChild(menuContainer)
-        //menuButton.appendChild(menuContainer)
+        menuContainer.style.backgroundColor= 'blue';
+        
+// Create a div for the first two images
+const DishesDiv = document.createElement('div');
+DishesDiv.classList.add('DishesDiv')
+const pastriesDiv = document.createElement('div');
+pastriesDiv.classList.add('pastriesDiv')
+const cocktailDiv = document.createElement('div');
+cocktailDiv.classList.add('cocktailDiv')
 
-
-
-var image1 = document.createElement("img");
-image1.src = "./static/images/banku.jpeg";
-image1.alt = "Banku: 40$";
-image1.style.width = '150px';
-image1.style.height = '150px'
-
-var image2 = document.createElement("img");
-image2.src = "./static/images/friedRice.jpeg";
-image2.alt = "Description for Image 2";
-
-var image3 = document.createElement("img");
-image3.src = "./static/images/friesss.jpeg";
-image3.alt = "Description for Image 3";
-
-var image4 = document.createElement("img");
-image4.src = "./static/images/fufuuu.jpeg";
-image4.alt = "Description for Image 4";
-
-var image5 = document.createElement("img");
-image5.src = "./static/images/gcb3.jpeg";
-image5.alt = "Description for Image 4";
-
-var image6 = document.createElement("img");
-image6.src = "./static/images/jollof.jpeg";
-image6.alt = "Description for Image 4";
-
-var image7 = document.createElement("img");
-image7.src = "./static/images/noodles.jpeg";
-image7.alt = "Description for Image 4";
-
-var image8 = document.createElement("img");
-image8.src = "./static/images/tuoZaafi.jpeg";
-image8.alt = "Description for Image 4";
-
-var image9 = document.createElement("img");
-image9.src = "./static/images/riceBalls.jpeg";
-image9.alt = "Description for Image 4";
-
-var image4 = document.createElement("img");
-image4.src = "./static/images/thaiNoodles.jpeg";
-image4.alt = "Description for Image 4";
-
-var image10 = document.createElement("img");
-image4.src = "./static/images/sushi.jpeg";
-image4.alt = "Description for Image 4";
-
-var image10 = document.createElement("img");
-image10.src = "./static/images/vegetablesSalad.jpeg";
-image10.alt = "Description for Image 4";
-
-var image11 = document.createElement("img");
-image11.src = "./static/images/briyani.jpeg";
-image11.alt = "Description for Image 4";
-
-var image12 = document.createElement("img");
-image12.src = "./static/images/burger.jpeg";
-image12.alt = "Description for Image 4";
-
-var image13 = document.createElement("img");
-image13.src = "./static/images/cake.jpeg";
-image13.alt = "Description for Image 4";
-
-var image14 = document.createElement("img");
-image14.src = "./static/images/cupcake.jpeg";
-image14.alt = "Description for Image 4";
-
-var image15 = document.createElement("img");
-image15.src = "./static/images/meatpie.jpeg";
-image15.alt = "Description for Image 4";
-
-var image16 = document.createElement("img");
-image16.src = "./static/images/pancake.jpeg";
-image16.alt = "Description for Image 4";
-
-var image17 = document.createElement("img");
-image17.src = "./static/images/pizza.jpeg";
-image17.alt = "Description for Image 4";
-
-var image18 = document.createElement("img");
-image18.src = "./static/images/waffles.jpeg";
-image18.alt = "Description for Image 4";
-
-var image19 = document.createElement("img");
-image19.src = "./static/images/summerPunchCocktail.jpg";
-image19.alt = "Description for Image 4";
-
-var image20 = document.createElement("img");
-image20.src = "./static/images/StrawberryCocktails.jpeg";
-image20.alt = "Description for Image 4";
-
-var image21 = document.createElement("img");
-image21.src = "./static/images/rainbowCocktail.jpeg";
-image21.alt = "Description for Image 4";
-
-var image22 = document.createElement("img");
-image22.src = "./static/images/milkshake.jpeg";
-image22.alt = "Description for Image 4";
-
-var image23 = document.createElement("img");
-image23.src = "./static/images/fruityCocktail.jpeg";
-image23.alt = "Description for Image 4";
-
-var image24 = document.createElement("img");
-image24.src = "./static/images/frozenVodka.jpeg";
-image24.alt = "Description for Image 4";
-
-var image25 = document.createElement("img");
-image25.src = "./static/images/bluelagoonCocktail.jpeg";
-image25.alt = "Description for Image 4";
-
-var image26 = document.createElement("img");
-image26.src = "./static/images/iceCream.jpeg";
-image26.alt = "Description for Image 4";
-
-menuContainer.appendChild(image1);
-menuContainer.appendChild(image2);
-menuContainer.appendChild(image3);
-menuContainer.appendChild(image4);
-menuContainer.appendChild(image5);
-menuContainer.appendChild(image6);
-menuContainer.appendChild(image7);
-menuContainer.appendChild(image8);
-menuContainer.appendChild(image9);
-menuContainer.appendChild(image10);
-menuContainer.appendChild(image11);
-menuContainer.appendChild(image12);
-menuContainer.appendChild(image13);
-menuContainer.appendChild(image14);
-menuContainer.appendChild(image15);
-menuContainer.appendChild(image16);
-menuContainer.appendChild(image17);
-menuContainer.appendChild(image18);
-menuContainer.appendChild(image19);
-menuContainer.appendChild(image20);
-menuContainer.appendChild(image21);
-menuContainer.appendChild(image22);
-menuContainer.appendChild(image23);
-menuContainer.appendChild(image24);
-menuContainer.appendChild(image25);
-menuContainer.appendChild(image26);
-
+menuContainer.appendChild(DishesDiv)
+menuContainer.appendChild(pastriesDiv)
+menuContainer.appendChild(cocktailDiv)
 
 document.body.appendChild(menuContainer);
 
-//});
+const imageUrls1 = [
+    './static/images/banku.jpeg',
+    './static/images/friedRice.jpeg',
+    './static/images/friesss.jpeg',
+    './static/images/fufuuu.jpeg',
+    './static/images/gcb3.jpeg',
+    './static/images/jollof.jpeg',];
+
+    const heading1 = document.createElement('h2')
+    heading1.textContent = "Local and Continental Dishes"
+    const headingDiv1 = document.createElement('div')
+    headingDiv1.classList.add('headingDiv1')
+    headingDiv1.appendChild(heading1)
+    DishesDiv.appendChild(headingDiv1)
+
+imageUrls1.forEach((imageUrl) => {
+  const imageDiv1 = document.createElement('div');
+  const image = document.createElement('img');
+  image.src = imageUrl;
+  image.alt = 'Image';
+  image.style.width = '150px';
+  image.style.height = '150px'
+
+  const text = document.createElement('p');
+  text.textContent = 'hello';
+
+  imageDiv1.appendChild(image);
+  imageDiv1.appendChild(text);
+  DishesDiv.appendChild(imageDiv1)
+});
+
+const imageUrls2 = [
+    './static/images/burger.jpeg',
+     './static/images/cake.jpeg',
+     './static/images/cupcake.jpeg',
+      './static/images/meatpie.jpeg',
+     './static/images/pancake.jpeg',
+    './static/images/pizza.jpeg', ];
+
+imageUrls2.forEach((imageUrl) => {
+  const imageDiv2 = document.createElement('div');
+  const image = document.createElement('img');
+  image.src = imageUrl;
+  image.alt = 'Image';
+  image.style.width = '150px';
+  image.style.height = '150px'
+
+  const text = document.createElement('p');
+  text.textContent = 'hello';
+
+  imageDiv2.appendChild(image);
+  imageDiv2.appendChild(text);
+
+   pastriesDiv.appendChild(imageDiv2)
+});
+
+const imageUrls3 = [
+   './static/images/summerPunchCocktail.jpg',
+   './static/images/StrawberryCocktails.jpeg',
+   './static/images/rainbowCocktail.jpeg',
+   './static/images/milkshake.jpeg',
+    './static/images/fruityCocktail.jpeg',
+   './static/images/frozenVodka.jpeg' ];
+
+imageUrls3.forEach((imageUrl) => {
+  const imageDiv3 = document.createElement('div');
+  const image = document.createElement('img');
+  image.src = imageUrl;
+  image.alt = 'Image';
+  image.style.width = '150px';
+  image.style.height = '150px'
+
+  const text = document.createElement('p');
+  text.textContent = 'hello';
+
+  imageDiv3.appendChild(image);
+  imageDiv3.appendChild(text);
+
+   cocktailDiv.appendChild(imageDiv3)
+});
+
 };
 
 export default menuPage;
